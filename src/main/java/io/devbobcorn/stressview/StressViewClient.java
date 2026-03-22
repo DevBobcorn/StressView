2b6ca429e0db21b140a086ab96b8cfb420767c28
Tasuku Bobcorn <devbobcorn@outlook.com>
2026-03-22 14:56:53 +0800

    :zap: Implement Flywheel visual for better performance

--- src/main/java/io/devbobcorn/stressview/StressViewClient.java
+++ src/main/java/io/devbobcorn/stressview/StressViewClient.java
@@ -1,8 +1,10 @@
 package io.devbobcorn.stressview;
 
 import io.devbobcorn.stressview.client.AdvancedStressometerRenderer;
+import io.devbobcorn.stressview.client.AdvancedStressometerVisual;
 import io.devbobcorn.stressview.client.StressViewPartialModels;
 
+import dev.engine_room.flywheel.lib.visualization.SimpleBlockEntityVisualizer;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
@@ -27,6 +29,11 @@ public class StressViewClient {
     static void onClientSetup(FMLClientSetupEvent event) {
         StressViewMod.LOGGER.info("HELLO FROM CLIENT SETUP");
         StressViewMod.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+
+        SimpleBlockEntityVisualizer.builder(StressViewMod.ADVANCED_STRESSOMETER_BE.get())
+                .factory(AdvancedStressometerVisual::new)
+                .skipVanillaRender(be -> true)
+                .apply();
     }
 
     static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
---
+++ src/main/java/io/devbobcorn/stressview/client/AdvancedStressometerVisual.java
@@ -0,0 +1,105 @@
+package io.devbobcorn.stressview.client;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.content.kinetics.base.ShaftVisual;
+
+import io.devbobcorn.stressview.block.AdvancedStressometerBlock;
+import io.devbobcorn.stressview.block.AdvancedStressometerBlockEntity;
+
+import dev.engine_room.flywheel.api.instance.Instance;
+import dev.engine_room.flywheel.api.visual.DynamicVisual;
+import dev.engine_room.flywheel.api.visualization.VisualizationContext;
+import dev.engine_room.flywheel.lib.instance.FlatLit;
+import dev.engine_room.flywheel.lib.instance.InstanceTypes;
+import dev.engine_room.flywheel.lib.instance.TransformedInstance;
+import dev.engine_room.flywheel.lib.model.Models;
+import dev.engine_room.flywheel.lib.transform.TransformStack;
+import dev.engine_room.flywheel.lib.visual.SimpleDynamicVisual;
+import net.createmod.catnip.data.Iterate;
+import net.minecraft.core.Direction;
+
+public class AdvancedStressometerVisual extends ShaftVisual<AdvancedStressometerBlockEntity>
+        implements SimpleDynamicVisual {
+
+    protected final ArrayList<HeadFace> faces = new ArrayList<>(2);
+    protected final PoseStack ms = new PoseStack();
+
+    public AdvancedStressometerVisual(VisualizationContext context,
+            AdvancedStressometerBlockEntity blockEntity, float partialTick) {
+        super(context, blockEntity, partialTick);
+
+        AdvancedStressometerBlock block = (AdvancedStressometerBlock) blockState.getBlock();
+
+        var headInstancer = instancerProvider().instancer(InstanceTypes.TRANSFORMED,
+                Models.partial(StressViewPartialModels.ADV_STRESSOMETER_HEAD));
+
+        var msr = TransformStack.of(ms);
+        msr.translate(getVisualPosition());
+
+        for (Direction facing : Iterate.directions) {
+            if (!block.shouldRenderHeadOnFace(level, pos, blockState, facing))
+                continue;
+
+            HeadFace face = new HeadFace(facing, headInstancer.createInstance());
+            faces.add(face);
+            face.setupTransform(msr);
+        }
+    }
+
+    @Override
+    public void beginFrame(DynamicVisual.Context ctx) {
+        // No animated parts yet; head instances are static.
+    }
+
+    @Override
+    public void updateLight(float partialTick) {
+        super.updateLight(partialTick);
+        relight(faces.stream()
+                .map(f -> f.instance)
+                .toArray(FlatLit[]::new));
+    }
+
+    @Override
+    protected void _delete() {
+        super._delete();
+        faces.forEach(HeadFace::delete);
+    }
+
+    @Override
+    public void collectCrumblingInstances(Consumer<Instance> consumer) {
+        super.collectCrumblingInstances(consumer);
+        for (HeadFace face : faces) {
+            consumer.accept(face.instance);
+        }
+    }
+
+    protected class HeadFace {
+        final Direction face;
+        final TransformedInstance instance;
+
+        HeadFace(Direction face, TransformedInstance instance) {
+            this.face = face;
+            this.instance = instance;
+        }
+
+        void setupTransform(TransformStack<?> msr) {
+            msr.pushPose();
+            rotateToFace(msr);
+            instance.setTransform(ms).setChanged();
+            msr.popPose();
+        }
+
+        TransformStack<?> rotateToFace(TransformStack<?> msr) {
+            return msr.center()
+                    .rotate((float) ((-face.toYRot() - 90) / 180 * Math.PI), Direction.UP)
+                    .uncenter();
+        }
+
+        void delete() {
+            instance.delete();
+        }
+    }
+}
