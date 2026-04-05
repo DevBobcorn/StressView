d2834e4778ae97f89601b1446fc187f26157d739
Tasuku Bobcorn <devbobcorn@outlook.com>
2026-04-05 18:48:29 +0800

    :bug: My tab shouldn't be inserted before vanilla ones...

--- src/main/java/io/devbobcorn/stressview/StressViewMod.java
+++ src/main/java/io/devbobcorn/stressview/StressViewMod.java
@@ -70,7 +70,7 @@ public class StressViewMod {
     // Creates a creative tab with the id "stressview:example_tab" for the example item, that is placed after the combat tab
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.stressview")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
+            // .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> ADVANCED_STRESSOMETER_ITEM.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
                 output.accept(ADVANCED_STRESSOMETER_ITEM.get()); // Add the Advanced Stressometer item to the tab. For your own tabs, this method is preferred over the event
